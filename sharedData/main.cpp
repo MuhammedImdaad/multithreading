163855e3fbbecf7defaee6e741b8cfdb7e46855b
Muhammed Imdaad <muhammedimdaad16@gmai.com>
2025-05-17 23:11:40 +0530

    shared data using atomic and mutex

---
+++ sharedData/main.cpp
@@ -0,0 +1,56 @@
+#include <thread>
+#include <iostream>
+#include <atomic>
+#include <mutex>
+
+void sharedIncrementI(std::atomic<int> &value)
+{
+    for (int i = 0; i < 1000; i++)
+        value++;
+}
+
+void usingAtomic()
+{
+    std::atomic<int> count;
+    count = 0;
+
+    std::thread t1(sharedIncrementI, std::ref(count));
+
+    std::thread t2(sharedIncrementI, std::ref(count));
+
+    t1.join();
+    t2.join();
+
+    std::cout << count << std::endl;
+}
+
+void sharedIncrementII(int &value, std::mutex& mtx)
+{
+    for (int i = 0; i < 1000; i++)
+    {
+        mtx.lock();
+        value++;
+        mtx.unlock();
+    }
+}
+
+void usingMutex()
+{
+    int count = 0;
+    std::mutex mtx;
+
+    std::thread t1(sharedIncrementII, std::ref(count), std::ref(mtx));
+
+    std::thread t2(sharedIncrementII, std::ref(count), std::ref(mtx));
+
+    t1.join();
+    t2.join();
+
+    std::cout << count << std::endl;
+}
+
+int main()
+{
+    usingAtomic();
+    usingMutex();
+}
